82279393f2a59d80c53a0cf6f5ab6023385393d9
Alvaro Mateos <alvaromah@hotmail.com>
2017-10-19 21:33:01 +0200

    Version 0.1.2.0

---
+++ src/eShop.Server/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Host = Request.Host.ToString();
+            return View();
+        }
+    }
+}
